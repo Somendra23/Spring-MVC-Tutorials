70a71fc506d756bc1aba2ab7b8b72b395aef357f
somendra23 <somendra23@gmail.com>
2024-10-20 13:23:01 -0400

    update proeprty files

--- Spring-Transactional/src/main/java/com/dailycodebuffer/transaction/config/Webconfig.java
+++ Spring-Transactional/src/main/java/com/dailycodebuffer/transaction/config/Webconfig.java
@@ -1,9 +1,12 @@
 package com.dailycodebuffer.transaction.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.dailycodebuffer.transaction.filter.TransactionFilter;
 import com.dailycodebuffer.transaction.interceptor.EmployeeInterceptor;
 
 @Configuration
@@ -14,4 +17,17 @@ public class Webconfig implements WebMvcConfigurer{
         registry.addInterceptor(new EmployeeInterceptor())
                 .addPathPatterns("/employee/**"); // Specify the URL patterns to apply the interceptor
     }
+
+
+@Bean
+public FilterRegistrationBean<TransactionFilter> registerFilters(){
+	
+	FilterRegistrationBean<TransactionFilter> tf = new FilterRegistrationBean<>();
+	tf.setFilter(new TransactionFilter());
+	tf.addUrlPatterns("/employee/*");
+	tf.setOrder(1);
+	
+	return tf;
+	
+}
 }
